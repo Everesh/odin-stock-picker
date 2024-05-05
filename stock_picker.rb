2e6d06faeaa5a077ca540454ad893c9b3a259abe
Everesh <everesh@proton.me>
2024-05-05 17:36:26 +0200

    Add logic

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,18 @@
 def stock_picker(arr)
 
-    # TO DO
+    max_margin = 0
+    tansaction_dates = [0,0]
+
+    arr.each_with_index do | buy_value, buy_date |
+        arr[buy_date..-1].each_with_index do | sell_value, sell_date |
+            if sell_value - buy_value > max_margin
+                max_margin = sell_value - buy_value
+                tansaction_dates[0] = buy_date
+                tansaction_dates[1] = sell_date + buy_date
+            end
+        end
+    end
+
+    tansaction_dates
 
 end
